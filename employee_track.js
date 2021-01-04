0d00658e3c488fd6e9fe7999484f8ccc3092b81f
Jake <jakekippes123gmail.com>
2021-01-04 17:11:14 -0600

    added the seeds for my database and added my initial prompt function for the app

--- employee_track.js
+++ employee_track.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
-const cTable = require("console.table");
-
+const consoleTable = require("console.table");
+//connects to the mysql database
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -9,8 +9,118 @@ const connection = mysql.createConnection({
   password: "Wow12345",
   database: "employee_trackerDB",
 });
-
+//throws and error if connection fails otherwise console logs the connection id and then starts the prompt function
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Connected as Id" + connection.threadId);
+  startPrompt();
 });
+//function that starts the prompt asking user what they would like to do next. Depending on the users choice it then goes to the correct function for that prompt.
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "choice",
+        choices: [
+          "View All Champions?",
+          "View All Champions By Class?",
+          "View all Champions By Race",
+          "Update Champion",
+          "Add Champion?",
+          "Add Class?",
+          "Add Race?",
+        ],
+      },
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View All Champions?":
+          viewAllChampions();
+          break;
+
+        case "View All Champions By Class?":
+          viewAllClasses();
+          break;
+        case "View all Champions By Race":
+          viewAllRaces();
+          break;
+
+        case "Update Champion":
+          updateChampion();
+          break;
+
+        case "Add Champion?":
+          addChampion();
+          break;
+
+        case "Add Class?":
+          addClass();
+          break;
+
+        case "Add Race?":
+          addRace();
+          break;
+      }
+    });
+}
+//this function allows you to
+function viewAllChampions() {
+  connection.query(
+    "SELECT champion.first_name, champion.last_name, role.title, role.salary, department.name, CONCAT(e.first_name, ' ' ,e.last_name) AS Manager FROM champion INNER JOIN role on role.id = champion.role_id INNER JOIN department on department.id = role.department_id left join champion e on champion.manager_id = e.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    }
+  );
+}
+//
+function viewAllClasses() {
+  connection.query(
+    "SELECT champion.first_name, champion.last_name, role.title AS Title FROM champion JOIN role ON champion.role_id = role.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    }
+  );
+}
+//
+function viewAllRaces() {
+  connection.query(
+    "SELECT champion.first_name, champion.last_name, department.name AS Department FROM champion JOIN role ON champion.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY champion.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.table(res);
+      startPrompt();
+    }
+  );
+}
+
+//
+let classArr = [];
+function selectclass() {
+  connection.query("SELECT * FROM class", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      classArr.push(res[i].title);
+    }
+  });
+  return classArr;
+}
+//
+let raidLeaderArr = [];
+function selectLeader() {
+  connection.query(
+    "SELECT first_name, last_name FROM champion WHERE raid_leader_id IS NULL",
+    function (err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        raidLeaderArr.push(res[i].first_name);
+      }
+    }
+  );
+  return raidLeaderArr;
+}
--- employee_tracker.sql
+++ employee_tracker.sql
@@ -1,6 +1,7 @@
 ALTER USER 'root'@'localhost'
 IDENTIFIED WITH mysql_nativepassword BY 'Wow12344';
 
+DROP DATABASE IF EXISTS employee_trackerDB;
 CREATE DATABASE employee_trackerDB;
 USE employee_tracker;
 
@@ -22,8 +23,50 @@ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 first_name VARCHAR(30),
 last_name VARCHAR(30),
 role_id INT,
-manager_id INT,
+raid_leader_id INT,
 FOREIGN KEY (role_id) REFERENCES role(id),
-FOREIGN KEY (manager_id) REFERENCES employee(id)
+FOREIGN KEY (raid_leader_id) REFERENCES employee(id)
 );
 
+INSERT INTO department (name)
+VALUE ("Orc");
+INSERT INTO department (name)
+VALUE ("Human");
+INSERT INTO department (name)
+VALUE ("Troll");
+INSERT INTO department (name)
+VALUE ("Dwarf");
+
+INSERT INTO role (title, salary, department_id)
+VALUE ("Paladin", 15000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Mage", 25000, 2);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Hunter", 17500, 4);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Warlock", 18000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Druid", 30000, 3);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Monk", 40000, 1);
+INSERT INTO role (title, salary, department_id)
+VALUE ("Warrior", 19000, 4);
+
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("John", "Smith", null, 1);
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("Koranes", "Rauoabjarnarstaoir", null, 4);
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("Krogran","Redbelly",null,3);
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("Kenneth", "Livingstone", 1, 2);
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("Issa", "Tree", 4, 5);
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("Shin-Young", "Windbrow", 1, 6);
+INSERT INTO employee (first_name, last_name, raid_leader_id, role_id)
+VALUE ("Myrduggs", "Hammerfall", 2, 7);
+
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
